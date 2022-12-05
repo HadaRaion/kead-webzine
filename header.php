69dd4359ec835095ab3edec7b664b9d500adb25c
hada <hadathing@gmail.com>
2022-12-05 00:57:50 -0800

    Git updater test.

--- header.php
+++ header.php
@@ -8,7 +8,7 @@
   <body <?php body_class(); ?>>
     <header class="site-header">
     <div class="container">
-      <h1 class="school-logo-text float-left"><a href="<?php echo site_url() ?>"><strong>Fictional</strong> University</a></h1>
+      <h1 class="school-logo-text float-left"><a href="<?php echo site_url() ?>">월간 장애인과 일터</a></h1>
       
     </div>
   </header>
