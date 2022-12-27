42f015ec6ca03c24fee8db3aaed04b391b09df98
hada <hadathing@gmail.com>
2022-12-27 11:28:00 -0800

    Add font-family in the them

--- functions.php
+++ functions.php
@@ -48,9 +48,9 @@ function noSubsAdminBar() {
   }
 }
 
-function tabor_setup() {
-add_theme_support( 'editor-styles' );
-add_editor_style( 'assets/css/style-editor.css' );
-}
+// function tabor_setup() {
+// add_theme_support( 'editor-styles' );
+// add_editor_style( 'assets/css/style-editor.css' );
+// }
 
-add_action( 'after_setup_theme', 'tabor_setup' );
+// add_action( 'after_setup_theme', 'tabor_setup' );
