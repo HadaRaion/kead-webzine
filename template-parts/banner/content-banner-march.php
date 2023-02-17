bb1a954628fa7a776a4a3d7192fcd006baf17f93
hada <hadathing@gmail.com>
2023-02-17 14:33:28 -0300

    Update march page

---
+++ template-parts/banner/content-banner-march.php
@@ -0,0 +1,22 @@
+<div class="cover-story cover-story--desktop t-center">
+  <div class="text-xl t-500">COVER STORY</div>
+  <h1 class="p-top-xs p-bottom-xs">파릇파릇, 설레는 변화</h1>
+  <p class="text-xl t-500">
+    삼한사온(三寒四溫)의 계절입니다. 변덕스러운 날씨에 움을 틔우려는 새싹들이 고개를 내밀까 말까 망설이는 때이기도</br>
+    하지요. 두려움과 호기심으로 도전의 시기를 고르는 것. 새순도 우리들도 봄을 맞이할 때 느끼는 설렘의 순간입니다.</br>
+    파릇파릇한 새봄, 여러분은 어떤 변화를 준비하고 계실까요?
+  </p>
+</div>
+<div class="cover-story cover-story--mobile t-center">
+  <div class="text-base t-500">COVER STORY</div>
+  <h1 class="p-top-xs p-bottom-small">파릇파릇, 설레는 변화</h1>
+  <p class="text-base t-500">
+    삼한사온(三寒四溫)의 계절입니다.</br>
+    변덕스러운 날씨에 움을 틔우려는 새싹들이</br>
+    고개를 내밀까 말까 망설이는 때이기</br>
+    하지요. 두려움과 호기심으로 도전의 시기를</br>
+    고르는 것. 새순도 우리들도 봄을 맞이할 때</br>
+    느끼는 설렘의 순간입니다. 파릇파릇한 새봄,</br>
+    여러분은 어떤 변화를 준비하고 계실까요?
+  </p>
+</div>
