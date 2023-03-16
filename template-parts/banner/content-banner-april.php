c0dfe1ef0e17fe5ddf1e583ba03c0a7c749ca0fa
hada <hadathing@gmail.com>
2023-03-16 17:29:24 -0300

    Complete the april page

---
+++ template-parts/banner/content-banner-april.php
@@ -0,0 +1,22 @@
+<div class="cover-story cover-story--desktop t-center">
+  <div class="text-xl t-500">COVER STORY</div>
+  <h1 class="p-top-xs p-bottom-xs">콩닥콩닥, 열정의 시간</h1>
+  <p class="text-xl t-500">
+    따사로운 기운이 온 세상을 감싸는 봄날입니다. 싹을 띄운 나무 새싹과 여린 꽃들이 세상을 향해 자신의 에너지를</br>
+    힘껏, 마음껏 내뿜는 시기이기도 하지요. 잠시 상상해보면 콩닥콩닥, 그 열정의 순간들에 얼마나 신이 날까요?</br>
+    여러분도 그런 특별한 봄날이시기를 기원합니다.
+  </p>
+</div>
+<div class="cover-story cover-story--mobile t-center">
+  <div class="text-base t-500">COVER STORY</div>
+  <h1 class="p-top-xs p-bottom-small">콩닥콩닥, 열정의 시간</h1>
+  <p class="text-base t-500">
+    따사로운 기운이 온 세상을 감싸는</br>
+    봄날입니다. 싹을 띄운 나무 새싹과</br>
+    여린 꽃들이 세상을 향해 자신의 에너지를</br>
+    힘껏, 마음껏 내뿜는 시기이기도 하지요.</br>
+    잠시 상상해보면 콩닥콩닥, 그 열정의</br>
+    순간들에 얼마나 신이 날까요? 여러분도</br>
+    그런 특별한 봄날이시기를 기원합니다.
+  </p>
+</div>
