9d29756c4b5749324c5de956ec0e787ec8b87ece
hada <hadathing@gmail.com>
2023-04-18 17:22:34 -0700

    Ready for may page

---
+++ template-parts/banner/content-banner-may.php
@@ -0,0 +1,23 @@
+<div class="cover-story cover-story--desktop t-center">
+  <div class="text-xl t-500">COVER STORY</div>
+  <h1 class="p-top-xs p-bottom-xs">몽글몽글, 감사의 마음</h1>
+  <p class="text-xl t-500">
+    푸른 하늘, 부드러운 공기, 곳곳에 핀 각양각색의 꽃들. 계절의 여왕 5월은 괜스레 기분이 좋아지고 행복한 마음이</br>
+    몽글몽글 피어오릅니다. 행복을 느낄 때 우리는 사랑하는 사람, 고마운 사람들을 떠올리게 되는데요.</br>
+    그래서일까요. 5월은 가정의 달입니다. 사랑과 감사함을 전하는 행복한 달이 되시길 바랍니다.
+  </p>
+</div>
+<div class="cover-story cover-story--mobile t-center">
+  <div class="text-base t-500">COVER STORY</div>
+  <h1 class="p-top-xs p-bottom-small">몽글몽글, 감사의 마음</h1>
+  <p class="text-base t-500">
+    푸른 하늘, 부드러운 공기, 곳곳에 핀</br>
+    각양각색의 꽃들. 계절의 여왕 5월은 괜스레</br>
+    기분이 좋아지고 행복한 마음이 몽글몽글</br>
+    피어오릅니다. 행복을 느낄 때 우리는</br>
+    사랑하는 사람, 고마운 사람들을 떠올리게</br>
+    되는데요. 그래서일까요. 5월은 가정의</br>
+    달입니다. 사랑과 감사함을 전하는</br>
+    행복한 달이 되시길 바랍니다.
+  </p>
+</div>
