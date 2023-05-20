6d1fed18410cbbe3a9a1f4dd49ac440c2186e61c
hada <sweet.raion@gmail.com>
2023-05-19 17:04:41 -0700

    Complete june page.

---
+++ template-parts/banner/content-banner-june.php
@@ -0,0 +1,23 @@
+<div class="cover-story cover-story--desktop t-center">
+  <div class="text-xl t-500">COVER STORY</div>
+  <h1 class="p-top-xs p-bottom-xs">다정다감, 배려의 힘</h1>
+  <p class="text-xl t-500">
+    어느덧, 싱그러운 초여름입니다. 덥지도 춥지도 않은 딱 기분 좋은 온도. 6월은 왠지 모르게 마음이 느긋해지기도</br>
+    하지요. 여유로운 계절의 기운 속에 가까운 이들과 평소 못다 한 이야기꽃을 피워보는 건 어떨까요?</br>
+    다정다감한 말과 소소한 배려로, 잔잔한 웃음꽃을 자주 피우는 향기로운 달이 되시길 빕니다.
+  </p>
+</div>
+<div class="cover-story cover-story--mobile t-center">
+  <div class="text-base t-500">COVER STORY</div>
+  <h1 class="p-top-xs p-bottom-small">다정다감, 배려의 힘</h1>
+  <p class="text-base t-500">
+    어느덧, 싱그러운 초여름입니다.</br>
+    덥지도 춥지도 않은 딱 기분 좋은 온도.</br>
+    6월은 왠지 모르게 마음이 느긋해지기도</br>
+    하지요. 여유로운 계절의 기운 속에 가까운</br>
+    이들과 평소 못다 한 이야기꽃을 피워보는 건</br>
+    어떨까요? 다정다감한 말과 소소한 배려로,</br>
+    잔잔한 웃음꽃을 자주 피우는 향기로운 달이</br>
+    되시길 빕니다.
+  </p>
+</div>
