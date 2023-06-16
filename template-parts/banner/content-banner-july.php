ebf7255d953718f021d7adfa1f8436342aa5c800
hada <sweet.raion@gmail.com>
2023-06-16 14:44:51 -0700

    Complete page for july

---
+++ template-parts/banner/content-banner-july.php
@@ -0,0 +1,23 @@
+<div class="cover-story cover-story--desktop t-center">
+  <div class="text-xl t-500">COVER STORY</div>
+  <h1 class="p-top-xs p-bottom-xs">새콤달콤, 힐링이 필요해</h1>
+  <p class="text-xl t-500">
+    7월은 휴가의 계절이지요. 한 해의 반을 열심히 달려온 우리가 손꼽아 기다리던 순간입니다. 햇빛에 반짝이는 바다,</br>
+    태양 아래 수영하는 사람들, 그늘 속의 시원한 바람, 계곡물에 담가둔 수박, 얼음 가득한 음료 한 잔, 느긋한 늦잠과</br>
+    읽고 싶었던 책…. 모두 다 좋습니다. 새콤달콤, 힐링의 시간을 맘껏 즐기세요!
+  </p>
+</div>
+<div class="cover-story cover-story--mobile t-center">
+  <div class="text-base t-500">COVER STORY</div>
+  <h1 class="p-top-xs p-bottom-small">새콤달콤, 힐링이 필요해</h1>
+  <p class="text-base t-500">
+    7월은 휴가의 계절이지요. 한 해의 반을</br>
+    열심히 달려온 우리가 손꼽아 기다리던</br>
+    순간입니다. 햇빛에 반짝이는 바다, 태양 아래</br>
+    수영하는 사람들, 그늘 속의 시원한 바람,</br>
+    계곡물에 담가둔 수박, 얼음 가득한</br>
+    음료 한 잔, 느긋한 늦잠과 읽고 싶었던 책….</br>
+    모두 다 좋습니다.</br>
+    새콤달콤, 힐링의 시간을 맘껏 즐기세요!
+  </p>
+</div>
