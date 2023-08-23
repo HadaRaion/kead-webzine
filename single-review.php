a92ca624192b666b77f1fad4161ff13b6e58bfce
hada <sweet.raion@gmail.com>
2023-08-23 15:59:03 -0700

    Complete september page

--- single-review.php
+++ single-review.php
@@ -11,36 +11,36 @@
 
 ?>
 
-  <main id="contents" class="single-post survey <?php echo $parentCategorySlug; ?>">
-    <section class="single-post__top">
-      <div class="single-post__top__wrapper">
-        <div class="single-post__top__title text-2xl ">
-          <div class="single-post__top__title__parent-cat font-bold"><?php echo $parentCategory ?></div>
-          <div class="single-post__top__title__cat"><?php echo $category ?></div>
-        </div>
-
-        <ul class="single-post__top__nav">
-          <li class="prev-link"><?php previous_post_link( '%link', '이전기사' ); ?></li>
-          <li class="next-link"><?php next_post_link( '%link', '다음기사' ); ?></li>
-        </ul>
+<main id="contents" class="single-post survey <?php echo $parentCategorySlug; ?>">
+  <section class="single-post__top">
+    <div class="single-post__top__wrapper">
+      <div class="single-post__top__title text-2xl ">
+        <div class="single-post__top__title__parent-cat font-bold"><?php echo $parentCategory ?></div>
+        <div class="single-post__top__title__cat"><?php echo $category ?></div>
       </div>
 
-    </section>
+      <ul class="single-post__top__nav">
+        <li class="prev-link"><?php previous_post_link( '%link', '이전기사' ); ?></li>
+        <li class="next-link"><?php next_post_link( '%link', '다음기사' ); ?></li>
+      </ul>
+    </div>
 
-    <?php while ( have_posts() ) : the_post();  ?>
+  </section>
 
-    <section class="single-post__contents p-top-large p-bottom-large">
-      <div class="container">
+  <?php while ( have_posts() ) : the_post();  ?>
 
-        <div class="page-banner p-bottom-large">
-          <h1 class="page-banner__title text-7xl">독자통신</h1>
-          <h2 class="page-banner__title text-6xl font-400">
-            독자 여러분의 의견을<span class="line-break"> 남겨주세요</span>
-          </h2>
-        </div>
+  <section class="single-post__contents p-top-large p-bottom-large">
+    <div class="container">
 
-        <div class="kead-reviews">
-          <?php 
+      <div class="page-banner p-bottom-large">
+        <h1 class="page-banner__title text-7xl">독자통신</h1>
+        <h2 class="page-banner__title text-6xl font-400">
+          독자 여러분의 의견을<span class="line-break"> 남겨주세요</span>
+        </h2>
+      </div>
+
+      <div class="kead-reviews">
+        <?php 
             $reviewer1 = get_field('kead-review-name-1');
             $reviewer2 = get_field('kead-review-name-2');
             $reviewer3 = get_field('kead-review-name-3');
@@ -50,62 +50,62 @@
             $review3 = get_field('kead-review-3');
             $review4 = get_field('kead-review-4');
           ?>
-          <?php if ( $reviewer1  ) : ?>
-          <ul class="p-bottom-large">
-            <li>
-              <div class="text-3xl font-bold kead-reviews__name"><?php echo $reviewer1 ?></div>
-              <p class="text-xl"><?php echo $review1 ?></p>
-            </li>
+        <?php if ( $reviewer1  ) : ?>
+        <ul class="p-bottom-large">
+          <li>
+            <div class="text-3xl font-bold kead-reviews__name"><?php echo $reviewer1 ?></div>
+            <p class="text-xl"><?php echo $review1 ?></p>
+          </li>
 
-            <?php if ( $reviewer2  ) : ?>
-            <li>
-              <div class="text-3xl font-bold kead-reviews__name"><?php echo $reviewer2 ?></div>
-              <p class="text-xl"><?php echo $review2 ?></p>
-            </li>
-            <?php endif; ?>
-
-            <?php if ( $reviewer3  ) : ?>
-            <li>
-              <div class="text-3xl font-bold kead-reviews__name"><?php echo $reviewer3 ?></div>
-              <p class="text-xl"><?php echo $review3 ?></p>
-            </li>
-            <?php endif; ?>
-
-            <?php if ( $reviewer4  ) : ?>
-            <li>
-              <div class="text-3xl font-bold kead-reviews__name"><?php echo $reviewer4 ?></div>
-              <p class="text-xl"><?php echo $review4 ?></p>
-            </li>
-            <?php endif; ?>
-          </ul>
+          <?php if ( $reviewer2  ) : ?>
+          <li>
+            <div class="text-3xl font-bold kead-reviews__name"><?php echo $reviewer2 ?></div>
+            <p class="text-xl"><?php echo $review2 ?></p>
+          </li>
           <?php endif; ?>
-        </div>
-        <div class="survey-form p-bottom">
-          <div class="survey-form__wrapper">
-            <h3 class="t-center text-4xl p-top">
-              [장애인과 일터] 독자 여러분의<br />
-              소중한 의견을 기다립니다
-            </h3>
-            <p class="t-center text-xl p-top-small">
-              재미있게 읽은 기사, 편집부에 바라는 점 등<br />
-              구독 후기를 남겨 주세요.
-            </p>
-            <div class="t-center text-base p-bottom-small">※이벤트에 응모하실 분은 내용 하단에 이름과 휴대폰 번호를 기재하셔야 기프티콘을 받으실 수 있습니다.</div>
-            <a class="btn btn--sm" href="<?php echo site_url('survey') ?>"><span>의견 남기기</span></a>
 
-          </div>
+          <?php if ( $reviewer3  ) : ?>
+          <li>
+            <div class="text-3xl font-bold kead-reviews__name"><?php echo $reviewer3 ?></div>
+            <p class="text-xl"><?php echo $review3 ?></p>
+          </li>
+          <?php endif; ?>
 
-          <img src="<?php echo get_theme_file_uri('/images/survey-form.svg') ?>" alt="편지를 주고 받는 사람들 일러스트">
-        </div>
+          <?php if ( $reviewer4  ) : ?>
+          <li>
+            <div class="text-3xl font-bold kead-reviews__name"><?php echo $reviewer4 ?></div>
+            <p class="text-xl"><?php echo $review4 ?></p>
+          </li>
+          <?php endif; ?>
+        </ul>
+        <?php endif; ?>
       </div>
-    </section>
-    
-    <?php endwhile; ?>
-    
-    <?php else : ?>
-    <p><?php _e( '기사가 존재하지 않습니다.' ); ?></p>
-    <?php endif; ?>
-  </main>
+      <div class="survey-form p-bottom">
+        <div class="survey-form__wrapper">
+          <h3 class="t-center text-4xl p-top">
+            [장애인과 일터] 독자 여러분의<br />
+            소중한 의견을 기다립니다
+          </h3>
+          <p class="t-center text-xl p-top-small">
+            재미있게 읽은 기사, 편집부에 바라는 점 등<br />
+            구독 후기를 남겨 주세요.
+          </p>
+          <div class="t-center text-base p-bottom-small">※이벤트에 응모하실 분은 내용 하단에 이름과 휴대폰 번호를 기재하셔야 기프티콘을 받으실 수 있습니다.</div>
+          <a class="btn btn--sm" href="<?php echo site_url('survey') ?>"><span>의견 남기기</span></a>
+
+        </div>
+
+        <img src="<?php echo get_theme_file_uri('/images/survey-form.svg') ?>" alt="편지를 주고 받는 사람들 일러스트">
+      </div>
+    </div>
+  </section>
+
+  <?php endwhile; ?>
+
+  <?php else : ?>
+  <p><?php _e( '기사가 존재하지 않습니다.' ); ?></p>
+  <?php endif; ?>
+</main>
 
 <?php
 get_sidebar();
---
+++ template-parts/banner/content-banner-september.php
@@ -0,0 +1,23 @@
+<div class="cover-story cover-story--desktop t-center">
+  <div class="text-xl t-500">COVER STORY</div>
+  <h1 class="p-top-xs p-bottom-xs">반짝반짝, 감수성 깨우기</h1>
+  <p class="text-xl t-500">
+    처서(處暑)를 지나 여름 더위가 한풀 꺾였습니다. 청명한 하늘은 한 뼘쯤 높아졌고요. 아침저녁 선선한 바람도</br>
+    가을의 시작을 느끼게 합니다. 계절의 영향일까요. 우리의 몸과 마음은 한결 차분해지고 쉬이 감성모드가 되기도</br>
+    하지요. 9월에는 가을을 맞아 우리의 감수성을 자극하는 다양한 문화예술을 많이 접해보시길 바랍니다.
+  </p>
+</div>
+<div class="cover-story cover-story--mobile t-center">
+  <div class="text-base t-500">COVER STORY</div>
+  <h1 class="p-top-xs p-bottom-small">반짝반짝, 감수성 깨우기</h1>
+  <p class="text-base t-500">
+    처서(處暑)를 지나 여름 더위가 한풀</br>
+    꺾였습니다. 청명한 하늘은 한 뼘쯤</br>
+    높아졌고요. 아침저녁 선선한 바람도 가을의</br>
+    시작을 느끼게 합니다. 계절의 영향일까요.</br>
+    우리의 몸과 마음은 한결 차분해지고 쉬이</br>
+    감성모드가 되기도 하지요. 9월에는 가을을</br>
+    맞아 우리의 감수성을 자극하는 다양한</br>
+    문화예술을 많이 접해보시길 바랍니다.
+  </p>
+</div>
