7cb33ede2bb967efd98340412259e7604af55c0d
hada <sweet.raion@gmail.com>
2023-09-17 17:11:54 -0700

    Add october page

---
+++ template-parts/banner/content-banner-october.php
@@ -0,0 +1,24 @@
+<div class="cover-story cover-story--desktop t-center">
+  <div class="text-xl t-500">COVER STORY</div>
+  <h1 class="p-top-xs p-bottom-xs">꿈틀꿈틀, 배움의 즐거움</h1>
+  <p class="text-xl t-500">
+    거리의 나뭇잎들이 빨간색, 주황색, 황금색으로 곱게 단풍물이 들었습니다. 어느덧 완연한 가을날이네요.</br>
+    시원한 공기와 맑은 하늘 아래, 알록달록 아름다운 자연 풍경들이 우리의 마음을 기분 좋고 더욱 풍요롭게 합니다.</br>
+    가을은 결실의 계절이기도 하지요. 여러분은 올 한해 어떤 배움을 통해 어떠한 결실을 맺으셨을지 문득 궁금해집니다.
+  </p>
+</div>
+<div class="cover-story cover-story--mobile t-center">
+  <div class="text-base t-500">COVER STORY</div>
+  <h1 class="p-top-xs p-bottom-small">꿈틀꿈틀, 배움의 즐거움</h1>
+  <p class="text-base t-500">
+    거리의 나뭇잎들이 빨간색, 주황색,</br>
+    황금색으로 곱게 단풍물이 들었습니다.</br>
+    어느덧 완연한 가을날이네요. 시원한 공기와</br>
+    맑은 하늘 아래, 알록달록 아름다운 자연</br>
+    풍경들이 우리의 마음을 기분 좋고 더욱</br>
+    풍요롭게 합니다. 가을은 결실의 계절이기도</br>
+    하지요. 여러분은 올 한해 어떤 배움을 통해</br>
+    어떠한 결실을 맺으셨을지</br>
+    문득 궁금해집니다.
+  </p>
+</div>
