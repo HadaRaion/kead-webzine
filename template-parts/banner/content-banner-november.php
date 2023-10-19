d6f7e9419c127fe1626a3be7d7fb4c108d5b8b20
hada <sweet.raion@gmail.com>
2023-10-18 19:21:22 -0700

    Add november page.

---
+++ template-parts/banner/content-banner-november.php
@@ -0,0 +1,24 @@
+<div class="cover-story cover-story--desktop t-center">
+  <div class="text-xl t-500">COVER STORY</div>
+  <h1 class="p-top-xs p-bottom-xs">조심조심, 안전은 필수</h1>
+  <p class="text-xl t-500">
+    어느새 늦가을입니다. 공기가 점점 더 쌀쌀해지면서 가을의 마지막 숨결을 느끼게 합니다. 나무는 알록달록 아름다웠던</br>
+    나뭇잎을 떨구고, 세상을 청명하게 비추던 햇빛도 많이 짧아졌습니다. 이러한 환절기일수록 건강을 유지하고 조심조심</br>
+    생활 속 안전을 지키는 것도 중요하겠지요? 막바지 가을날을 무탈하고 여유롭게, 온기 넘치는 겨울도 맞으시기를.
+  </p>
+</div>
+<div class="cover-story cover-story--mobile t-center">
+  <div class="text-base t-500">COVER STORY</div>
+  <h1 class="p-top-xs p-bottom-small">조심조심, 안전은 필수</h1>
+  <p class="text-base t-500">
+    어느새 늦가을입니다. 공기가 점점 더</br>
+    쌀쌀해지면서 가을의 마지막 숨결을</br>
+    느끼게 합니다. 나무는 알록달록 아름다웠던</br>
+    나뭇잎을 떨구고, 세상을 청명하게 비추던</br>
+    햇빛도 많이 짧아졌습니다. 이러한</br>
+    환절기일수록 건강을 유지하고 조심조심</br>
+    생활 속 안전을 지키는 것도 중요하겠지요?</br>
+    막바지 가을날을 무탈하고 여유롭게,</br>
+    온기 넘치는 겨울도 맞으시기를.
+  </p>
+</div>
---
+++ template-parts/event/content-event-november.php
@@ -0,0 +1,21 @@
+<div class="article__event p-top">
+  <div class="article__event__text">
+    <h2 class="text-7xl">웹진 보고 선물 받자</h2>
+    <p class="text-2xl font-bold p-top-small p-bottom-small">
+      [장애인과 일터] 기사 읽고 독자 코너 참여하고<br />
+      기분 좋은 선물 받아 가세요!
+    </p>
+    <!-- Event 링크 교체 -->
+    <a class="btn btn--sm" href="<?php echo site_url('/november/event'); ?>">이벤트 바로가기</a>
+    <div class="article__event__text__period  p-top">
+      <div class="t-underline text-xl font-bold">이벤트 기간</div>
+      <div class="text-xl p-bottom-xs">2023년 11월 1일(수) ~ 11월 22일(수)</div>
+      <div class="t-underline text-xl font-bold">당첨자 발표</div>
+      <div class="text-xl p-bottom-small">개별 발송 및 [장애인과 일터] 12월호 이벤트 페이지에 공지</div>
+    </div>
+
+  </div>
+  <div class="article__event__image">
+    <img src="<?php echo get_theme_file_uri('/images/event-thumb-may.svg'); ?>" alt="기프티콘 이미지">
+  </div>
+</div>
